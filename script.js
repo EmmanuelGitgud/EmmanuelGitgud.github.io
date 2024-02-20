630502f3dc420624d17d1497bf6931de503dcb25
EmmanuelGitgud <92885074+EmmanuelGitgud@users.noreply.github.com>
2024-02-20 19:08:08 +0800

    made nav bar responsive and added a sidebar for mobile

--- script.js
+++ script.js
@@ -1,38 +1,12 @@
-function refreshPage(){
-    window.location = window.location.href;
-}
-//setInterval('refreshPage()', 1000)
-// Get references to the menu toggle button and the navigation bar
+const hamburgerNav = document.getElementById("hamburger-nav");
+const closeNav = document.getElementById("close-nav");
+const sidebar = document.getElementById("sidebar");
 
-const menuToggle = document.getElementById('menu-toggle');
-const navBar = document.getElementById('nav-bar');
-const navLabel = document.getElementById('nav-label');
-const mainContent = document.getElementById('main-content');
-
-// Add click event listener to the menu toggle button
-menuToggle.addEventListener('click', function() {
-    // Toggle the 'active' class on the menu toggle button
-    this.classList.toggle('active');
-    
-    // Toggle the 'active' class on the navigation bar to show/hide it
-    navBar.classList.toggle('active');
-});
-
-// Function to check screen size and show/hide hamburger menu
-function checkScreenSize() {
-    if (window.innerWidth <= 1200) {
-        menuToggle.style.display = 'block';
-        navBar.style.display = 'none';
-        navLabel.style.fontSize = '1.5rem';
-        mainContent.style.margin = '12px';
-    } else {
-        menuToggle.style.display = 'none';
-        navBar.style.display = 'block';
-        navLabel.style.fontSize = '2.5rem';
-        mainContent.style.margin = '24px';
-    }
+function toggleSidebar(){
+    sidebar.classList.toggle("hide");
 }
 
-// Check screen size on page load and resize
-window.addEventListener('load', checkScreenSize);
-window.addEventListener('resize', checkScreenSize);
+
+
+hamburgerNav.addEventListener("click", toggleSidebar);
+closeNav.addEventListener("click", toggleSidebar);
