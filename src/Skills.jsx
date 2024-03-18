6bb3fb9d8998c2e593b82279ce1da83d7ec97118
EmmanuelGitgud <92885074+EmmanuelGitgud@users.noreply.github.com>
2024-03-19 03:37:05 +0800

    Edited Skills page
    edited skills page to fit tablets and other for factors

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -19,13 +19,13 @@ function MySkills(){
                     <img src="c-sharp.png" alt="html"/>
                     <img src="unity.png" alt="html"/>
                 </div>
-                <div className='description-wrapper'>
+            </div>
+            <div className='description-wrapper'>
                     <h1>&lt;My Skills/&gt;</h1>
                     <hr/>
                     <h2>
                     My skill set encompasses a diverse range of both front-end and back-end frameworks and technologies, allowing me to develop comprehensive solutions across various platforms. With proficiency in HTML5, CSS3, and JavaScript, I specialize in crafting visually appealing and responsive user interfaces. On the back end, I excel in languages such as PHP and Python, leveraging them alongside databases like MySQL to create robust and efficient server-side functionality. Additionally, I'm well-versed in version control systems like Git, ensuring seamless collaboration and code management. My expertise extends to content management systems such as WordPress, where I adeptly customize and extend functionality to meet clients' specific needs. Furthermore, my experience with programming languages like C# and frameworks like Unity enables me to explore the realm of game development, adding a creative dimension to my skill set. Overall, my skills empower me to build dynamic and innovative solutions that address complex challenges and deliver exceptional user experiences.
                     </h2>
-                </div>
             </div>
         </div>
         </>
