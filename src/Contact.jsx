feb1a9c119ec4ee7554f6eb44d5fc28694f0e346
EmmanuelGitgud <92885074+EmmanuelGitgud@users.noreply.github.com>
2024-03-22 17:10:29 +0800

    Update Contact.jsx

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -14,7 +14,7 @@ function ContactMe(){
                         <h2>Email: contact@emmanarabit.online</h2>   
                         <h2>Github: <a href="https://github.com/EmmanuelGitgud" target='_blank'>EmmanuelGitgud</a></h2>
                     </div>
-                    <form className='form-wrapper' name='contact' netlify>
+                    <form className='form-wrapper' name='contact' method='post' netlify>
                         <input type="text" name='name' placeholder='Name' required/>
                         <input type="email" name='email' placeholder='Email' required/>
                         <textarea name="message" cols="30" rows="10" placeholder='Message' required/>
